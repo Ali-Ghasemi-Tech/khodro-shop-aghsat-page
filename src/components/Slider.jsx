d3b4a4eccf413aee379898435e7384a432e039fd
TLB30 <thelastblade30@gmail.com>
2023-11-23 17:30:49 +0330

    day 2 make the slider and put its realtime vlaue inside a variable

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Slider = () => {
+  const [value, setValue] = useState(1)
+
+  let input =document.getElementById('input')
+  function handelChange(e) {
+    setValue(e.target.value)
+  }
   return (
-    <div>Slider</div>
+    <div className='flex flex-col gap-10 justify-center w-full items-center my-11'>
+      <div>
+        {value}
+      </div>
+      <input id='input'value={value} onChange={handelChange} type="range" min="1" max="500" className="w-[50%] appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:w-[50%] [&::-webkit-slider-runnable-track]:h-3 [&::-webkit-slider-runnable-track]:bg-black/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[42px] [&::-webkit-slider-thumb]:w-[150px] [&::-webkit-slider-thumb]:-mt-4 [&::-webkit-slider-thumb]:rounded-full background-svg hover:cursor-pointer" 
+      />
+      
+    </div>
   )
 }
 
